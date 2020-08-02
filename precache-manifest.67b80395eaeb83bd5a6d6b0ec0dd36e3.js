b43e7ec95008c8759bcc339c028dce84a2e1d1ae
teobot <theoclapperton@outlook.com>
2020-08-02 15:53:53 +0100

    Updates

---
+++ precache-manifest.67b80395eaeb83bd5a6d6b0ec0dd36e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "504a8bfd28f1b8fafd25855fea86d3b7",
+    "url": "/acnh-animal-helper/index.html"
+  },
+  {
+    "revision": "d7c5892fd13508d1e753",
+    "url": "/acnh-animal-helper/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2bcc2f732e6c79dacf6b",
+    "url": "/acnh-animal-helper/static/css/main.d4774912.chunk.css"
+  },
+  {
+    "revision": "d7c5892fd13508d1e753",
+    "url": "/acnh-animal-helper/static/js/2.d34e9b57.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/acnh-animal-helper/static/js/2.d34e9b57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bcc2f732e6c79dacf6b",
+    "url": "/acnh-animal-helper/static/js/main.5451ec3e.chunk.js"
+  },
+  {
+    "revision": "73bff028a205f0a54866",
+    "url": "/acnh-animal-helper/static/js/runtime-main.cd39ae30.js"
+  }
+]);
